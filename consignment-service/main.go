b79c05b375650aff7a4904eb7f0b3f747c7e9588
chapin <chapin@class100.com>
2018-12-07 11:38:53 +0800

    update

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"github.com/micro/go-micro"
 	pb "shippy/consignment-service/proto/consignment"
+	vesselPb "shippy/vessel-service/proto/vessel"
 )
 
 
@@ -28,9 +29,25 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 
 type service struct {
 	repo Repository
+	vesselClient vesselPb.VesselServiceClient
 }
 
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, resp *pb.Response) error {
+	
+	
+	vReq := &vesselPb.Specification{
+		Capacity: int32(len(req.Containers)),
+		MaxWeight: req.Weight,
+	}
+
+	vResp, err := s.vesselClient.FindAvailable(context.Background(), vReq)
+	if err != nil {
+		return nil
+	}
+
+	log.Printf("found vessel: %s\n", vResp.Vessel.Name)
+
+	req.VesselId = vResp.Vessel.Id
 	consignment, err := s.repo.Create(req)
 	if err != nil {
 		return err
@@ -42,7 +59,6 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, re
 }
 
 func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, resp *pb.Response) error {
-	log.Printf("%t", resp)
 	allConsignments := s.repo.GetAll()
 	resp.Consignments = allConsignments
 	return nil
@@ -59,8 +75,9 @@ func main() {
 	 server.Init()
 
 	 repo := Repository{}
-
-	 pb.RegisterShippingServiceHandler(server.Server(), &service{ repo })
+	 vClient := vesselPb.NewVesselServiceClient("go.micro.srv.vessel", server.Client())
+	
+	 pb.RegisterShippingServiceHandler(server.Server(), &service{ repo, vClient })
 
 	 if err := server.Run(); err != nil {
 		log.Fatalf("failed to serve: %v", err)
